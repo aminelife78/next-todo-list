96bc6e338f46d0abed10e30251cd7b9caf118394
med amine <aminelife86@gmail.com>
2024-11-14 08:55:59 +0100

    problm prisma

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,43 +1,36 @@
-import { PrismaClient } from '@prisma/client'
-import { faker } from '@faker-js/faker';
+import { PrismaClient } from "@prisma/client";
+// import { faker } from '@faker-js/faker';
 
-
-
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 async function main() {
   // create todo
-  await prisma.todo.createMany({
-    data: Array.from({length:5}, ()=>{
-      return {
-        title: faker.book.title(),
-        body: faker.lorem.text(),
-        completed:faker.datatype.boolean(),
-        user_id:"jlkjljljl"
-              
-      }
-    })
-  })
-
-
+  // await prisma.todo.createMany({
+  //   data: Array.from({length:5}, ()=>{
+  //     return {
+  //       title: faker.book.title(),
+  //       body: faker.lorem.text(),
+  //       completed:faker.datatype.boolean(),
+  //       user_id:"jlkjljljl"
+  //     }
+  //   })
+  // })
   // create users
   // await prisma.user.createMany({
   //   data: Array.from({length:20}, ()=>{
   //     return {
   //       name: faker.name.firstName(),
   //       email: faker.internet.email(),
-        
   //     }
   //   })
   // })
-
 }
 
 main()
   .catch(async (e) => {
-    console.error(e)
-    process.exit(1)
+    console.error(e);
+    process.exit(1);
   })
   .finally(async () => {
-    await prisma.$disconnect()
-  })
+    await prisma.$disconnect();
+  });
